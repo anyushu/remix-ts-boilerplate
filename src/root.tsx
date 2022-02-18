b37f68190e31b2682289fddd0c3d9e8ce77ae8a5
anyushu <suzuki@lamp.jp>
2022-02-18 17:26:05 +0900

    install tailwindcss

--- src/root.tsx
+++ src/root.tsx
@@ -1,5 +1,10 @@
 import { Meta, Links, Outlet, ScrollRestoration, Scripts, LiveReload } from 'remix'
-import type { MetaFunction } from 'remix'
+import type { MetaFunction, LinksFunction } from 'remix'
+import styles from '~/styles/app.css'
+
+export const links: LinksFunction = () => {
+  return [{ rel: 'stylesheet', href: styles }]
+}
 
 export const meta: MetaFunction = () => {
   return { title: 'New Remix App' }
